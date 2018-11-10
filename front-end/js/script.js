ced4bafed98596ed0b5ea8f4d437f996cfda937b
Indrajit Sarkar <indrajit.sarkar@digitaslbi.com>
2018-11-10 22:44:20 +0530

    FE - quote page

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -3,7 +3,8 @@ $(function(){
 
 	var openNav = $('.open'),
 		closeNav = $('.closebtn'),
-		navPanel = $('#mySidenav');
+		navPanel = $('#mySidenav'),
+		parters = $('.logo-slider');
 
 
 	$(openNav).on('click', function(e){
@@ -15,5 +16,38 @@ $(function(){
 		e.preventDefault();
 		$(navPanel).css('left', '-250px');
 	});
+
+	if (parters.length) {
+		parters.slick({
+			arrows: false,
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			autoplay: true,
+  			autoplaySpeed: 3000,
+  			responsive: [
+			    {
+			      breakpoint: 1024,
+			      settings: {
+			        slidesToShow: 3,
+			        slidesToScroll: 3
+			      }
+			    },
+			    {
+			      breakpoint: 600,
+			      settings: {
+			        slidesToShow: 2,
+			        slidesToScroll: 2
+			      }
+			    },
+			    {
+			      breakpoint: 480,
+			      settings: {
+			        slidesToShow: 1,
+			        slidesToScroll: 1
+			      }
+			    }
+			  ]
+		});
+	}
 
 });
