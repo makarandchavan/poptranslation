67ddbf3430b68057bf8f8d30ca1db3464e745bc8
Makarand Chavan <cmak2008@gmail.com>
2018-12-10 01:45:27 +0530

    All Drupal files and theme integration

---
+++ themes/photo/js/script.js
@@ -0,0 +1,182 @@
+var Drupal = Drupal || {};
+
+(function ($, Drupal, Bootstrap) {
+	"use strict";
+
+	var openNav = $('.open'),
+		closeNav = $('.closebtn'),
+		navPanel = $('#mySidenav'),
+		parters = $('.logo-slider'),
+		bannerSlider= $('.banners'),
+		dropzone = $('#dropzone'),
+		topicSlider = $('.topics');
+		
+
+	$('[data-toggle="popover"]').popover();
+	
+	$(openNav).on('click', function(e){
+		e.preventDefault();
+		$(navPanel).css('left', '0px');
+	});
+
+	$(closeNav).on('click', function(e){
+		e.preventDefault();
+		$(navPanel).css('left', '-250px');
+	});
+
+	if ($('#wizard').length) {
+		$('#wizard').steps({
+	        headerTag: 'h3',
+	        bodyTag: 'section',
+	        transitionEffect: 'slideLeft',
+	        onStepChanged: function (event, currentIndex, priorIndex){
+		        if (currentIndex === 5) {
+		        	var previewNode = document.querySelector("#template");
+						previewNode.id = "";
+
+					var previewTemplate = previewNode.parentNode.innerHTML;
+						previewNode.parentNode.removeChild(previewNode);
+
+					var myDropzone = new Dropzone(document.body, {
+					  url: "/target-url", // Set the url
+					  thumbnailWidth: 40,
+					  thumbnailHeight: 40,
+					  parallelUploads: 20,
+					  maxFilesize: 20, // MB
+					  previewTemplate: previewTemplate,
+					  //autoQueue: false, // Make sure the files aren't queued until manually added
+					  previewsContainer: "#previews" // Define the container to display the previews
+					  // clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
+					});
+		        }
+
+		        if (currentIndex === 6) {
+		        	$('.actions').hide();
+		        }
+		    }
+	    });
+	}
+	
+	if (topicSlider.length) {
+		topicSlider.slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			infinite: false,
+			nextArrow: '<button type="button" class="slick-next ti-arrow-circle-right"></button>',
+			prevArrow: '<button type="button" class="slick-prev ti-arrow-circle-left"></button>'
+		});
+	}
+
+	if (bannerSlider.length) {
+		bannerSlider.slick({
+			arrows: false,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: true,
+  			autoplaySpeed: 3000
+		});
+	}
+
+	if (parters.length) {
+		parters.slick({
+			arrows: false,
+			slidesToShow: 2,
+			slidesToScroll: 1,
+			autoplay: true,
+  			autoplaySpeed: 1000,
+  			responsive: [
+			    {
+			      breakpoint: 1024,
+			      settings: {
+			        slidesToShow: 2,
+			        slidesToScroll: 1
+			      }
+			    },
+			    {
+			      breakpoint: 600,
+			      settings: {
+			        slidesToShow: 2,
+			        slidesToScroll: 1
+			      }
+			    },
+			    {
+			      breakpoint: 480,
+			      settings: {
+			        slidesToShow: 1,
+			        slidesToScroll: 1
+			      }
+			    }
+			  ]
+		});
+	}
+
+
+	// File Upload with Dropzone
+
+	var dropWrapper = document.getElementsByClassName("dzone");
+
+	if (dropWrapper.length) {
+		
+		var previewNode = document.querySelector("#template");
+			previewNode.id = "";
+
+		var previewTemplate = previewNode.parentNode.innerHTML;
+			previewNode.parentNode.removeChild(previewNode);
+
+		var myDropzone = new Dropzone(document.body, {
+		  url: "/sites/default/files", // Set the url
+		  thumbnailWidth: 40,
+		  thumbnailHeight: 40,
+		  parallelUploads: 20,
+		  maxFilesize: 20, // MB
+		  previewTemplate: previewTemplate,
+		  //autoQueue: false, // Make sure the files aren't queued until manually added
+		  previewsContainer: "#previews", // Define the container to display the previews
+		  clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
+		});
+
+	}
+
+	// Slide In Panel
+	
+	var panelTriggers = document.getElementsByClassName('js-cd-panel-trigger');
+	if( panelTriggers.length > 0 ) {
+		for(var i = 0; i < panelTriggers.length; i++) {
+			(function(i){
+				var panelClass = 'js-cd-panel-'+panelTriggers[i].getAttribute('data-panel'),
+					panel = document.getElementsByClassName(panelClass)[0];
+				// open panel when clicking on trigger btn
+				panelTriggers[i].addEventListener('click', function(event){
+					event.preventDefault();
+					addClass(panel, 'cd-panel--is-visible');
+				});
+				//close panel when clicking on 'x' or outside the panel
+				panel.addEventListener('click', function(event){
+					if( hasClass(event.target, 'js-cd-close') || hasClass(event.target, panelClass)) {
+						event.preventDefault();
+						removeClass(panel, 'cd-panel--is-visible');
+					}
+				});
+			})(i);
+		}
+	}
+	
+	//class manipulations - needed if classList is not supported
+	//https://jaketrent.com/post/addremove-classes-raw-javascript/
+	function hasClass(el, className) {
+	  	if (el.classList) return el.classList.contains(className);
+	  	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+	}
+	function addClass(el, className) {
+	 	if (el.classList) el.classList.add(className);
+	 	else if (!hasClass(el, className)) el.className += " " + className;
+	}
+	function removeClass(el, className) {
+	  	if (el.classList) el.classList.remove(className);
+	  	else if (hasClass(el, className)) {
+	    	var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+	    	el.className=el.className.replace(reg, ' ');
+	  	}
+	}
+
+})(window.jQuery, window.Drupal, window.Drupal.bootstrap);
