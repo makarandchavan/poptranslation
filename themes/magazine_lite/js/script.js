3492f4b7e7cc56cc656fdf068f4f5a82ce727d10
Makarand Chavan <cmak2008@gmail.com>
2019-01-12 22:14:26 +0530

    Added magazine theme

---
+++ themes/magazine_lite/js/script.js
@@ -0,0 +1,21 @@
+var Drupal = Drupal || {};
+
+(function ($, Drupal, Bootstrap) {
+	"use strict";
+
+	var openNav = $('.open'),
+		closeNav = $('.closebtn'),
+		navPanel = $('#mySidenav');
+
+	
+	$(openNav).on('click', function(e){
+		e.preventDefault();
+		$(navPanel).css('left', '0px');
+	});
+
+	$(closeNav).on('click', function(e){
+		e.preventDefault();
+		$(navPanel).css('left', '-250px');
+	});
+
+})(window.jQuery, window.Drupal, window.Drupal.bootstrap);
